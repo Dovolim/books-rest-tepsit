20efba97da613105d349c8b98a88f699e00a523b
Dovolim <160941276+Dovolim@users.noreply.github.com>
2026-04-30 11:58:21 +0200

    Update RestApplication.java

--- RestApplication.java
+++ RestApplication.java
@@ -1,21 +1,15 @@
 package it.edu.marconipontedera.tepsit.rest;
-
 import org.glassfish.jersey.server.ResourceConfig;
-
 import javax.ws.rs.ApplicationPath;
-
 /**
- * Classe di configurazione dell'applicazione JAX-RS.
- * Definisce il path base "/api" per tutti gli endpoint REST.
- *
+ * Classse di configurazione dell'applicazione JAX-RS.
+ * Definisce il path base "/api" per tutti gli endpoint REST
  * Tutti i servizi saranno accessibili sotto:
  *   http://localhost:8080/api/books
  */
 @ApplicationPath("/api")
 public class RestApplication extends ResourceConfig {
-
     public RestApplication() {
-        // Scansione automatica del package per trovare le risorse JAX-RS
         packages("it.edu.marconipontedera.tepsit.rest");
     }
 }

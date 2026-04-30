b205f7079026aa8c3fc1946ea698d3c024a3d153
Dovolim <160941276+Dovolim@users.noreply.github.com>
2026-04-30 12:17:01 +0200

    Update CORSFilter.java

--- CORSFilter.java
+++ CORSFilter.java
@@ -7,12 +7,6 @@ import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-/**
- * Filtro JAX-RS per gestire le intestazioni CORS (Cross-Origin Resource Sharing).
- * Permette al client di comunicare con il server anche se gira su porte diverse.
- *
- * Basato sulla lezione 5 della guida html.it: "Filtri e listener".
- */
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
 
@@ -21,7 +15,7 @@ public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin",  "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Origgin",  "*");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
         LOGGER.fine("CORS headers aggiunti per: " + requestContext.getUriInfo().getRequestUri());
